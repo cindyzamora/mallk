3883692bf26527560ce64bb51e0b47a31697ccd0
Cindy Zamora <cindy@bluekite.com>
2015-02-17 20:08:54 -0600

    share store detail

--- app/src/main/java/com/example/cindy/mallk/app/StoreDetail.java
+++ app/src/main/java/com/example/cindy/mallk/app/StoreDetail.java
@@ -1,5 +1,6 @@
 package com.example.cindy.mallk.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.util.Linkify;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class StoreDetail extends ActionBarActivity {
@@ -44,12 +46,26 @@ public class StoreDetail extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Intent i = getIntent();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_share:
+                String name = i.getStringExtra("NAME");
+                String msg = getString(R.string.message, name);
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(intent.EXTRA_TEXT, msg);
+                intent.setType("text/plain");
+
+                startActivity(Intent.createChooser(intent,getString(R.string.share)));
+                return true;
+            case R.id.action_settings:
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+//        return super.onOptionsItemSelected(item);
     }
 }
